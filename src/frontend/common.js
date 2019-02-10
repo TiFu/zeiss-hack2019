2975fecdcd5f463ff71e508829d036a69f9f45c5
Tino Fuhrmann <tino-fuhrmann@web.de>
2019-02-10 05:40:06 +0100

    Added moar frontend code

---
+++ src/frontend/common.js
@@ -0,0 +1,43 @@
+function dateFormat(dateString) {
+    if (dateString == null) {
+        return "";
+    }
+    const m = new Date(dateString)
+    let formatted = (makeTwoDigit(m.getUTCDate())) +"."+ (makeTwoDigit(m.getUTCMonth()+1)) +"."+ m.getUTCFullYear() + " " + makeTwoDigit(m.getUTCHours()) + ":" + makeTwoDigit(m.getUTCMinutes()) + ":" + makeTwoDigit(m.getUTCSeconds());
+    return formatted;
+}
+
+function makeTwoDigit(number) {
+    if (number >= 10) {
+        return number;
+    } else {
+        return "0" + number.toString()
+    }
+}
+
+let formatDate = dateFormat;
+
+function getBadgeForEntry(quality) {
+    if (quality >= 1) {
+        return "success"
+    } else if (quality >= 0) {
+        return "warning"
+    } else {
+        return "danger"
+    }
+}
+
+function numberFormat(number) {
+    if (number == null) {
+        return (0).toFixed(2);
+    }
+    return Number.parseFloat(number).toFixed(2);
+}
+
+function angleFormat(number) {
+    if (number == null) {
+        return (0).toFixed(2) + "°";
+    } else {
+        return (Number.parseFloat(number) * 180.0 / Math.PI).toFixed(2) + "°"
+    }
+}
---
+++ src/frontend/details.js
@@ -0,0 +1,44 @@
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+
+
+$(document).ready(function() {
+    $.getJSON("http://bec160b4.ngrok.io/analysis-images/" + getUrlParameter("id"), function (data) {
+        $("#id-header").html("Sample " + data["picture_id"]);
+
+        $("#overlay_left").attr("src", data["overlay_left"].replace("http://localhost:8765", "http://bec160b4.ngrok.io"));
+        $("#overlay_right").attr("src", data["overlay_right"].replace("http://localhost:8765", "http://bec160b4.ngrok.io"))
+
+        $("#left_before").attr("src", data["picture_left_before"].replace(".tif", ".png"))
+        $("#left_after").attr("src", data["picture_left_after"].replace(".tif", ".png"))
+        $("#right_before").attr("src", data["picture_right_before"].replace(".tif", ".png"))
+        $("#right_after").attr("src", data["picture_right_after"].replace(".tif", ".png"))
+        $("#time").html("<b>Date:</b> " + dateFormat(data["date_before"]) + " - " + dateFormat(data["date_after"]))
+        console.log(data)
+        $("#transform_left").html("(" + numberFormat(data["translation_val_left_x"]) + ", " + numberFormat(data["translation_val_left_y"]) + ")")
+        $("#transform_right").html("(" + numberFormat(data["translation_val_right_x"]) + ", " + numberFormat(data["translation_val_right_y"]) + ")")
+        $("#rotation_left").html(angleFormat(data["rotation_val_left"]))
+        $("#rotation_right").html(angleFormat(data["rotation_val_right"]))
+
+        badge = getBadgeForEntry(data["quality"])
+        $("#quality_score").html("<b>Quality Score:</b> <span style=\"margin-left: 5px\" class=\"badge badge-" + badge + " quality_score\">" + numberFormat(data["quality"]) + "</span>")
+        console.log("received data", data)
+        $("#loading-spinner").hide();
+        $("#content").show();
+
+    }); 
+})
+
